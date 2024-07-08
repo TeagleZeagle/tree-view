fc20b7a17a1ebfc63b9a930552a23c017b2300ac
Kaan Kani İnce <kkince@newmind.ai>
2024-07-08 10:22:07 +0300

    tree-view created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Tree view component/menu UI component/ recursive navigation menu */}
+      <TreeView menus={menus}/>
+    </div>
+  );
+}
+
+export default App;
